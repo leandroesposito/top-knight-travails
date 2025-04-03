d15494af4898e17ee74be7c7d4c96af3599f39e8
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-04-03 17:51:46 -0300

    Implement knightMoves() function usin BFS

--- knightMoves.js
+++ knightMoves.js
@@ -1,3 +1,50 @@
-import LinkedList from "LinkedList.js";
+import LinkedList from "./LinkedList.js";
 
-export default function knightMoves(start, dest) {}
+export default function knightMoves(start, dest) {
+  const visited = new Set();
+  const queue = new LinkedList();
+  queue.append({
+    pos: start,
+    path: [],
+  });
+
+  // BFS path search
+  while (queue.size() > 0) {
+    const queueSize = queue.size();
+
+    // process current iteration
+    for (let i = 0; i < queueSize; i++) {
+      let { pos, path } = queue.removeAt(0).value;
+
+      // validate position
+      if (pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) {
+        continue;
+      }
+
+      // visit position
+      const posStr = `${pos[0]},${pos[1]}`;
+      if (visited.has(posStr)) {
+        continue;
+      }
+      visited.add(posStr);
+
+      // update path
+      path = [...path, pos];
+      if (pos[0] == dest[0] && pos[1] == dest[1]) {
+        return path;
+      }
+
+      // append reachable position to exploration queue
+      const [x, y] = pos;
+      queue.append({ pos: [x - 1, y - 2], path });
+      queue.append({ pos: [x - 1, y + 2], path });
+      queue.append({ pos: [x + 1, y - 2], path });
+      queue.append({ pos: [x + 1, y + 2], path });
+      queue.append({ pos: [x - 2, y + 1], path });
+      queue.append({ pos: [x - 2, y - 1], path });
+      queue.append({ pos: [x + 2, y + 1], path });
+      queue.append({ pos: [x + 2, y - 1], path });
+    }
+  }
+  return [];
+}
